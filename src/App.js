c28a4b19fba7c23d743ab0541b5606ab5c703153
Aayushman4725 <aayushmannp@gmail.com>
2025-06-09 14:41:06 +0545

    added Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import NavBar from "./NavBar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Create from "./Create";
 
 function App() {
   return (
-   
+   <Router>
     <div className="content">
        <NavBar></NavBar>
       <h1>App Component</h1>
-      <Home />
+      <Switch>
+        <Route exact path = "/">
+          <Home />
+        </Route>
+        <Route path = "/create">
+          <Create />
+        </Route>
+
+      </Switch>
     </div>
+    </Router>
   );
 }
 
